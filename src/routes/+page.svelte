3ac3753f80f8c42761ab8e73318393d00577000d
Aryxon04 <76776871+AryxonDazmor@users.noreply.github.com>
2023-05-16 08:00:00 +0000

    fix structure

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<script lang ="ts">
+</script>
+
+<a href="/Fixtures"><img src="https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Manchester_City_FC_badge.svg/1200px-Manchester_City_FC_badge.svg.png"></a>
---
+++ src/routes/Fixtures/+page.svelte
@@ -0,0 +1,2 @@
+<p>Upcoming games for Nugget</p>
+<a href="/">Home</a>
